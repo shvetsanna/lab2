d34ef73db203bc75ea2a645caca6f2ab22f835c0
shvetsanna <shvetsanna139@gmail.com>
2026-03-13 21:56:06 +0000

    Add files via upload

---
+++ mathUtils.js
@@ -0,0 +1,17 @@
+ class MathUtils {
+
+  add(a, b) {
+    return a + b;
+  }
+
+  subtract(a, b) {
+    return a - b;
+  }
+
+  square(a) {
+    return a * a;
+  }
+
+}
+
+module.exports = MathUtils;
