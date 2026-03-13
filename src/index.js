d4892d9e4c33850a64c6a239162a8e6df3ddd583
shvetsanna <shvetsanna139@gmail.com>
2026-03-13 22:25:31 +0000

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+ const MathUtils = require("./mathUtils");
+const StringUtils = require("./stringUtils");
+
+module.exports = {
+  MathUtils,
+  StringUtils
+};
---
+++ src/stringUtils.js
@@ -0,0 +1,17 @@
+ class StringUtils {
+
+  toUpper(text) {
+    return text.toUpperCase();
+  }
+
+  toLower(text) {
+    return text.toLowerCase();
+  }
+
+  length(text) {
+    return text.length;
+  }
+
+}
+
+module.exports = StringUtils;
